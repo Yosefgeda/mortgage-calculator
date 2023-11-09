c240aac7a92b3297e98500e1219d2332fcef830e
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-11-09 15:28:09 +0300

    imported Navbar from materialUi

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Mortgage Calculator
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
-
 export default Navbar
