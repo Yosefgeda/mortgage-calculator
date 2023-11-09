ded45bdaf79ab21b3c2cd32290db4baf2bec4203
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-11-09 15:09:19 +0300

    Dark mode for all app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import darkTheme from './theme';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
