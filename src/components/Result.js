3b8f1af5e12296d0575e5ef0e710934eeb694d91
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-11-14 16:27:13 +0300

    Tenure select monthly payment

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,6 +3,19 @@ import React from 'react'
 const Result = ({data}) => {
   const { homeValue, downPayment, loanAmount, loanTerm, interestRate } = data
   
+  const totalLoanMonths = loanTerm * 12;
+  const interestPerMonth = interestRate / 100 / 12;
+
+  const monthlyPayment = (
+    loanAmount * interestPerMonth *
+    (1 + interestPerMonth) ** totalLoanMonths
+  ) / 
+  (
+    (1 + interestPerMonth) ** totalLoanMonths - 1
+  );
+
+  const totalInterestGenerated = monthlyPayment + totalLoanMonths + loanAmount;
+  
   return (
     <div>Result</div>
   )
