53ad5860da44d7e1d77ca98b9b0998cc7ab0adba
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-11-14 16:35:46 +0300

    Tenure select pie chart data

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,4 +1,10 @@
-import React from 'react'
+import { Stack, Typography } from '@mui/material';
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 
 const Result = ({data}) => {
   const { homeValue, downPayment, loanAmount, loanTerm, interestRate } = data
@@ -15,9 +21,40 @@ const Result = ({data}) => {
   );
 
   const totalInterestGenerated = monthlyPayment + totalLoanMonths + loanAmount;
+
+  const pieChartData = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
   
   return (
-    <div>Result</div>
+    <Stack>
+      <Typography textAlign='center' variant='h5'>
+        Monthly Payment: $ {monthlyPayment.toFixed(2)}
+      </Typography>
+    </Stack>
   )
 }
 
